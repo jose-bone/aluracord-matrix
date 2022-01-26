baca4244438753597e7f59a7f9b7e625544c5806
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-01-26 21:31:22 +0200

    Application page

---
+++ pages/_app.js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: "Open Sans", sans-serif;
+      }
+      /* App fit Height */
+      html,
+      body,
+      #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */
+    `}</style>
+  );
+}
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title>Aluracord - Matrix</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Aluracord" key="title" />
+        <meta
+          property="og:description"
+          content="Entre em um chat para conversar."
+        />
+        <meta
+          property="og:url"
+          content="https://aluracord-jose-bone.vercel.app"
+        />
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
+}
